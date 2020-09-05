7d6d02ee42ece22dccc763f42a695c8f02a1880c
Eugene <deppodim@gmail.com>
2020-09-05 08:29:03 +0300

    second task added

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -10,9 +10,7 @@ const weapons = [
 ];
 
 function getNames() {
-  const names = [];
-  weapons.forEach(weapon => names.push(weapon.name));
-  return names;
+  return weapons.map(weapon => weapon.name));
 }
 
 function getCountReliableWeapons(durability) {
@@ -38,3 +36,43 @@ function getTotalDamage() {
   }, 0);
   return totalDamage;
 }
+
+// Задача 2
+
+function sleep(milliseconds) {
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+}
+
+function sum(...args) {
+  sleep(100);
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
+}
+
+function compareArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return arr1.every(index => arr1[index] === arr2[index]);
+  } else {
+    return false;
+  }
+}
+
+function memorize(fn, limit) {
+  const memory = [];
+  
+  function newFunction (arguments) {
+    const found = memory.find(obj => compareArrays(obj.args, arguments));
+    if (found) {
+      return found.result;
+    } else {
+      memory.push({args: arguments, result: fn(arguments)});
+      if (memory.length > limit) {
+        memory.pop();
+      }
+    };
+  };
+
+  return fn();
+};
